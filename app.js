0b9d8238a2a5055cd3f3ac7687c15d122c401c01
palooara <araozpaloma15@gmail.com>
2025-07-14 23:07:26 -0300

    Integrador II

---
+++ app.js
@@ -0,0 +1,47 @@
+//Configuramos nuestro servidor
+//1. Librerías
+
+const express = require('express'); //framework para crear servidores
+const morgan = require('morgan');// middleware para registrar las peticiones HTTP
+const hbs = require('hbs');// motor de plantillas para renderizar html
+const path = require('path'); //librería para trabajar con rutas
+
+//2.Creamos el servidor
+const app = express(); 
+
+//3.Aplicamos los middlewares
+app.use(morgan('dev')); //registrar las peticiones HTTP en la consola
+app.use(express.json()); //parsear el cuerpo de las peticiones a JSON
+let contactos = []; //array para almacenar los contactos
+
+app.use(express.urlencoded({ extended: true })); //parsear el cuerpo de las peticiones a URL-encoded
+
+app.use(express.static(path.join(__dirname, 'public'))); //servir archivos estáticos desde la carpeta public
+
+//4.Configuramos el motor de plantillas
+app.set('view engine', 'hbs');
+
+// 5. Configuramos las rutas de hbs
+app.set('views', path.join(__dirname, 'views')); //configurar la carpeta de vistas
+
+// 6. Importamos las rutas
+const pagesRouter = require('./routes/pagesRouter');
+
+//7. Usamos las rutas
+app.use('/', pagesRouter);
+
+//8. Middleware para manejar errores 404 y 500
+// Middleware para manejar rutas no encontradas
+app.use((req, res) =>{
+    console.log('Ruta no encontrada:' + req.url);
+    res.status(404).send('<h1>404-Página no encontrada</h1>');
+});
+
+// Middleware para manejar errores internos del servidor
+app.use((err, req, res, next) => {
+    console.error( err.stack);
+    res.status(500).send('<h1>500-Error interno del servidor</h1>');
+});
+
+//Exportamos el servidor
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,20 @@
+const dotenv =require('dotenv');
+dotenv.config();
+
+//importamos la config del servidor
+const app = require('./app');
+
+//Importamos la conexión a la base de datos
+const connectDB = require('./conexion/conecctionMongo');
+
+//Importamos el puerto del archivo .env
+const PORT = process.env.PORT || 3000; // Puerto por defecto o el especificado en el archivo .env
+
+//Importamos la URL de conexión a la base de datos
+const MONGO_URI = process.env.MONGO_LOCAL;
+
+connectDB(MONGO_URI);
+
+app.listen(PORT,() => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
